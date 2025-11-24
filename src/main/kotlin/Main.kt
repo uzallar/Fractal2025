12fc36e9f05e1cd26ee35b55f4b3b84cb941f131
macbook <serbakovd187@gmail.com>
2025-11-24 20:37:58 +0300

    удалил комментарий

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -43,9 +43,9 @@ fun main(): Unit = application {
         App()
     }
     student2Print()
-    student1Print()
-    student3Print()
 }
+
+// пробный коммит
 
 
 private fun student2Print() {
@@ -55,19 +55,3 @@ private fun student2Print() {
     println("Student 2: $x * $y = $product")
 }
 
-
-private fun student1Print() {
-    val a = 5
-    val b = 3
-    val sum = a + b
-    println("Student 1: $a + $b = $sum")
-}
-
-
-
-private fun student3Print() {
-    val a = 15
-    val b = 4
-    val difference = a - b
-    println("Student 3: $a - $b = $difference")
-}
