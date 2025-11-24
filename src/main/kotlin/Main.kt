2d93bc7dfedd9e208e231528b354e0171a55f422
Malvina <k.malvina555@gmail.com>
2025-11-24 21:25:19 +0300

    еще один коммит чтобы понят как мерджить в свою ветку

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,7 +12,7 @@ import app.viewmodels.MainViewModel
 
 @Composable
 @Preview
-fun App(viewModel: MainViewModel = MainViewModel()) {
+fun App(viewModel: MainViewModel= MainViewModel()) {
     MaterialTheme {
         Box {
             PaintPanel(
@@ -44,6 +44,9 @@ fun main(): Unit = application {
     }
     student2Print()
 }
+
+// пробный коммит
+// что-то еще
 
 private fun student2Print() {
     val x = 7
