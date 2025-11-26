2a1a49a2ca4e758feea0095a4679bd0583f5d169
stepa_aaa <pivnoykaban@gmail.com>
2025-11-26 22:32:32 +0300

    Добавил функцию student9Print и ее вызов (Main.kt)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.window.application
 import app.ui.PaintPanel
 import app.ui.SelectionPanel
 import app.viewmodels.MainViewModel
-import kotlin.math.pow
 
 @Composable
 @Preview
@@ -44,18 +43,11 @@ fun main(): Unit = application {
         App()
     }
     student2Print()
-    student1Print()
-    student3Print()
-    student5Print()
-    student6Print()
-    student8Print()
+    student9Print()
 }
 
 // пробный коммит
-// что-то еще
-// еще что то пропро
-//пвапвапавпвапвап55
-// пупупу2
+
 
 private fun student2Print() {
     val x = 7
@@ -65,37 +57,8 @@ private fun student2Print() {
 }
 
 
-
-private fun student1Print() {
-    val a = 5
-    val b = 3
-    val sum = a + b
-    println("Student 1: $a + $b = $sum")
+private fun student9Print() {
+    val randomNum = (1..100).random()
+    println("Student 9: Random number between 1 and 100: $randomNum")
 }
 
-private fun student3Print() {
-    val a = 15
-    val b = 4
-    val difference = a - b
-    println("Student 3: $a - $b = $difference")
-}
-
-private fun student5Print() {
-    val base = 2
-    val exponent = 5
-    val power = base.toDouble().pow(exponent.toDouble()).toInt()
-    println("Student 5: $base^$exponent = $power")
-//привет привет :)
-}
-
-private fun student6Print() {
-    val dividend = 17
-    val divisor = 5
-    val remainder = dividend % divisor
-    println("Student 6: $dividend % $divisor = $remainder")
-}
-
-private fun student8Print() {
-    val pi = Math.PI
-    println("Student 8: PI = ${"%.4f".format(pi)}")
-}
