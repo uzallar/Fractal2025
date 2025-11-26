4083e50dfcf2bd8e2418ca7acc2d538b38d3a5e5
анна08122 <shakshina109099@yandex.ru>
2025-11-26 17:09:58 +0300

    hello

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.window.application
 import app.ui.PaintPanel
 import app.ui.SelectionPanel
 import app.viewmodels.MainViewModel
-import kotlin.math.pow
 
 @Composable
 @Preview
@@ -45,16 +44,8 @@ fun main(): Unit = application {
     }
     student2Print()
     student1Print()
-    student3Print()
-    student5Print()
-    student6Print()
 }
 
-// пробный коммит
-// что-то еще
-// еще что то пропро
-//пвапвапавпвапвап55
-// пупупу2
 
 private fun student2Print() {
     val x = 7
@@ -64,32 +55,9 @@ private fun student2Print() {
 }
 
 
-
 private fun student1Print() {
     val a = 5
     val b = 3
     val sum = a + b
     println("Student 1: $a + $b = $sum")
-}
-
-private fun student3Print() {
-    val a = 15
-    val b = 4
-    val difference = a - b
-    println("Student 3: $a - $b = $difference")
-}
-
-private fun student5Print() {
-    val base = 2
-    val exponent = 5
-    val power = base.toDouble().pow(exponent.toDouble()).toInt()
-    println("Student 5: $base^$exponent = $power")
-//привет привет :)
-}
-
-private fun student6Print() {
-    val dividend = 17
-    val divisor = 5
-    val remainder = dividend % divisor
-    println("Student 6: $dividend % $divisor = $remainder")
 }
