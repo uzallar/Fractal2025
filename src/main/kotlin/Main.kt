bf959bff0f94c8763f9c5dfee4d1d15673900307
Islam_Karimov <Karimovislam797@gmail.com>
2025-11-27 10:35:41 +0300

    добавил student8Print

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.window.application
 import app.ui.PaintPanel
 import app.ui.SelectionPanel
 import app.viewmodels.MainViewModel
+import kotlin.math.pow
 
 @Composable
 @Preview
@@ -43,6 +44,11 @@ fun main(): Unit = application {
         App()
     }
     student2Print()
+    student1Print()
+    student3Print()
+    student5Print()
+    student6Print()
+    student8Print()
     student9Print()
 }
 
@@ -62,3 +68,38 @@ private fun student9Print() {
     println("Student 9: Random number between 1 and 100: $randomNum")
 }
 
+
+
+private fun student1Print() {
+    val a = 5
+    val b = 3
+    val sum = a + b
+    println("Student 1: $a + $b = $sum")
+}
+
+private fun student3Print() {
+    val a = 15
+    val b = 4
+    val difference = a - b
+    println("Student 3: $a - $b = $difference")
+}
+
+private fun student5Print() {
+    val base = 2
+    val exponent = 5
+    val power = base.toDouble().pow(exponent.toDouble()).toInt()
+    println("Student 5: $base^$exponent = $power")
+//привет привет :)
+}
+
+private fun student6Print() {
+    val dividend = 17
+    val divisor = 5
+    val remainder = dividend % divisor
+    println("Student 6: $dividend % $divisor = $remainder")
+}
+
+private fun student8Print() {
+    val pi = Math.PI
+    println("Student 8: PI = ${"%.4f".format(pi)}")
+}
