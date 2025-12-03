d544947a88e3eac28e94e3bbe410749815fed137
bluestar12-hub <shilnikna@mail.ru>
2025-12-03 23:14:59 +0300

    добавила функции отмены по шагам тесты для нее и показ координат актульных по нахождению мышки

---
+++ src/main/kotlin/app/mouse/CoordinatesTracker.kt
@@ -0,0 +1,14 @@
+//package app.mouse
+//
+//import androidx.compose.runtime.*
+//import androidx.compose.ui.geometry.Offset
+//
+//@Stable
+//class CoordinatesTracker {
+//    var mousePosition by mutableStateOf<Offset?>(null)
+//        private set
+//
+//    fun updateMousePosition(position: Offset?) {
+//        mousePosition = position
+//    }
+//}
